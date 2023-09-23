58b4cc4f2e503c3aac57ddf044c03707b924cf8b
sbroadhurst <stepbro31@gmail.com>
2023-09-23 17:58:20 -0400

    remove code that wasnt supposed to be there and readd icons

--- pages/games.js
+++ pages/games.js
@@ -20,25 +20,6 @@ const Games = () => {
           )
         })}
       </CardRow>
-
-      <div style="display:flex;">
-        <div style="display: flex; flex-direction: column; width: fit-content; border:1px solid black; text-align:center; ">
-          <div style="border-bottom: 1px solid black; padding: 5px ; background-color:pink ">gear_id</div>
-          <div style="padding: 5px  ">gear_ball_1</div>
-        </div>
-        <div style="display: flex; flex-direction: column;  border:1px solid black; text-align:center;  ">
-          <div style="border-bottom: 1px solid black; padding: 5px; background-color:pink ">level - (10)</div>
-          <div style="padding: 5px; display: flex; align-items:center; ">
-            <input type="number" style="width:100%; text-align:center;  border:none   " />
-          </div>
-        </div>
-        <div style="display: flex; flex-direction: column; width: 100px; border:1px solid black; text-align:center; ">
-          <div style="border-bottom: 1px solid black; padding: 5px;background-color:pink ">shards - (666)</div>
-          <div style="padding: 5px; display: flex; align-items:center; ">
-            <input type="number" style="width:100%; text-align:center; border:none " />
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
